6dbec17b428c8095665ff96cd5f09bacface9006
William Banham <will_banh@icloud.com>
2025-01-08 15:54:18 +0000

    (v:0.0.3) Moved colour echos to functions (not tested, whoops)

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -12,15 +12,31 @@ HOSTNAME=hostname
 IP=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1 | head -n 1)
 USERHOST="$(whoami)@$(hostname)"
 
+notice() {
+    echo -e "${GREY}$1${RESET}\n"
+}
+
+info() {
+    echo -e "${BLUE}$1${RESET}\n"
+}
+
+warn() {
+    echo -e "${YELLOW}$1${RESET}\n"
+}
+
+success() {
+    echo -e "${GREEN}$1${RESET}\n"
+}
+
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}This script must be run as sudo. Please re-run with sudo.${RESET}" 
    exit 1
 fi
 
-echo -e "${GREY}Running with sudo privileges...\n${RESET}"
+warn "Running with sudo privileges..."
 
 # Notify on finish
-echo -e "${YELLOW}do you wanna get notifed on finish? [Y/n]${RESET}"
+warn "do you wanna get notifed on finish? [Y/n]"
 read NOTFY_FINISH
 
 NOTFY_FINISH=$(echo "$NOTFY_FINISH" | tr '[:upper:]' '[:lower:]')
@@ -31,34 +47,34 @@ fi
 
 # Updates etc...
 sudo apt update -y && sudo apt upgrade -y
-echo -e "${GREEN}successfully updated system\n${RESET}"
+success "successfully updated system"
 
 # Install qemu-guest-agent
-echo -e "${BLUE}Setting up qemu-guest-agent\n\n${RESET}"
+info "Setting up qemu-guest-agent\n"
 sudo apt-get install qemu-guest-agent -y && sudo systemctl enable --now qemu-guest-agent
-echo -e "${GREEN}successfully installed and started the qemu-guest-agent\n${RESET}"
+success "successfully installed and started the qemu-guest-agent"
 
 # Install base packages
-echo -e "${BLUE}Setting up base packages (git, ufw...)\n\n${RESET}"
+info "Setting up base packages (git, ufw...)\n"
 sudo apt-get install git ufw
-echo -e "${GREEN}successfully installing base packages\n${RESET}"
+success "successfully installing base packages"
 
 # Expose ssh port
-echo -e "${BLUE}Exposing ssh port 22\n\n${RESET}"
+info "Exposing ssh port 22"
 sudo ufw enable && sudo ufw allow 22
-echo -e "${GREEN}successfully exposed port 22\n${RESET}"
+success "successfully exposed port 22"
 
 # Tailwcale
-echo -e "${BLUE}Setting up tailscale\n\n${RESET}"
-echo -e "${GREY}provide tailscale auth key: ${RESET}" 
+info "Setting up tailscale"
+notice "provide tailscale auth key: " 
 read TS_AUTH_KEY
 
 curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=$TS_AUTH_KEY
 TS_IP=$(tailscale ip | grep -E "^[0-9]+\.")
-echo -e "${GREEN}successfully setup tailscale${RESET}${GREY} - ${TS_IP}\n${RESET}"
+success "successfully setup tailscale${RESET}${GREY} - ${TS_IP}"
 
 # Setup docker
-echo -e "${YELLOW}do you wanna setup docker? [Y/n]${RESET}"
+warn "do you wanna setup docker? [Y/n]"
 read DOCKER_SETUP
 
 DOCKER_SETUP=$(echo "$DOCKER_SETUP" | tr '[:upper:]' '[:lower:]')
@@ -68,10 +84,10 @@ if [ -z "$DOCKER_SETUP" ]; then
 fi
 
 if [ "$DOCKER_SETUP" == "y" ]; then
-    echo -e "${BLUE}Setting up docker\n\n${RESET}"
+    info "setting up docker"
 
     # Add Docker's official GPG key:
-    echo -e "${GREY}adding dockers offical GPG key...${RESET}"
+    notice "adding dockers offical GPG key..."
     sudo apt-get update
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
@@ -79,7 +95,7 @@ if [ "$DOCKER_SETUP" == "y" ]; then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    echo -e "${GREY}adding apt sources...${RESET}"
+    notice "adding apt sources..."
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -87,41 +103,41 @@ if [ "$DOCKER_SETUP" == "y" ]; then
     sudo apt-get update -y
 
     # Install docker
-    echo -e "${GREY}installing docker via APT...${RESET}"
+    notice "installing docker via APT..."
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
     # Rootless docker
-    echo -e "${GREY}rootless docker setup...${RESET}"
+    notice "rootless docker setup..."
     sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker
 
     # Start on boot (just incase)
-    echo -e "${GREY}setting up docker start on boot (just incase)...\n${RESET}"
+    notice "setting docker to start on boot (just incase)..."
     sudo systemctl enable docker && sudo systemctl enable containerd
 
-    echo -e "${GREEN}docker installed\n\n${RESET}"
+    success "docker installed"
 elif [ "$user_input" == "n" ]; then
-    echo -e "${GREY}not installing docker${RESET}"
+    notice "not installing docker"
 else
-    echo -e "${GREY}assuming default: no${RESET}"
+    notice "assuming default: no"
 fi
 
 # Setting up beszel monitoring
-echo -e "${BLUE}Setting up beszel monitoring agent\n\n${RESET}"
+info "Setting up beszel monitoring agent"
 
-echo -e "${GREY}enter beszel monitoring ssh key: ${RESET}"
+notice "enter beszel monitoring ssh key: "
 read BESZEL_SSH_KEY
 
 curl -sL https://raw.githubusercontent.com/henrygd/beszel/main/supplemental/scripts/install-agent.sh -o install-agent.sh && chmod +x install-agent.sh && ./install-agent.sh -p 45876 -k "$BESZEL_SSH_KEY"
 sudo ufw allow 45876
-echo -e "${GREEN}beszel setup - add it in the dashboard ${RESET}${GREY}(https://monitor.banham.info)\n\n${RESET}"
+success "beszel setup - add it in the dashboard ${RESET}${GREY}\"$IP\" (https://monitor.banham.info)"
 
 
 if [ "$NOTFY_FINISH" == "y" ]; then
     read -p "NTFY Username: " NTFY_USR
     read -p "NTFY Password: " NTFY_PWD
 
-    curl -u "$NTFY_USR:$NTFY_PWD" -d "Service is finish setting up - ${USERNAME} (LOCAL: ${IP}/TS: ${TS_IP})\n\nRebooting" ntfy.banham.info/system_notfy
+    curl -u "$NTFY_USR:$NTFY_PWD" -d "Service is finish setting up (rebooting next) - ${USERNAME} (LOCAL: ${IP}/TS: ${TS_IP})" ntfy.banham.info/system_notfy
 fi
 
-echo -e "\n\n\n${YELLOW}rebooting...\n${RESET}"
+warn "\n\nrebooting..."
 sudo reboot
